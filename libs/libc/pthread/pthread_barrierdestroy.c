apache/nuttx

libc/pthread: return EBUSY when barrier is in use
MIRTOS-114

Add check for semcount==0 before barrier destroy and initialization

Change-Id: Ifa0cdb363714ad15eafa5ef49a025520ce836b1f
Signed-off-by: Peter Bee <bijunda1@xiaomi.com>

libs/libc/pthread/pthread_barrierdestroy.c
@@ -77,6 +77,7 @@
 int pthread_barrier_destroy(FAR pthread_barrier_t *barrier)
 {
   int ret = OK;
+  int semcount;
 
   if (!barrier)
     {
@@ -84,8 +85,16 @@ int pthread_barrier_destroy(FAR pthread_barrier_t *barrier)
     }
   else
     {
-      sem_destroy(&barrier->sem);
-      barrier->count = 0;
+      nxsem_get_value(&barrier->sem, &semcount);
+      if (semcount == 0)
+        {
+          sem_destroy(&barrier->sem);
+          barrier->count = 0;
+        }
+      else
+        {
+          ret = EBUSY;
+        }
     }
 
   return ret;

xiaoxiang781216/incubator-nuttx

libs/libc/pthread/pthread_barrierdestroy.c
@@ -62,6 +62,7 @@
 int pthread_barrier_destroy(FAR pthread_barrier_t *barrier)
 {
   int ret = OK;
+  int semcount;
 
   if (!barrier)
     {
@@ -69,8 +70,16 @@ int pthread_barrier_destroy(FAR pthread_barrier_t *barrier)
     }
   else
     {
-      sem_destroy(&barrier->sem);
-      barrier->count = 0;
+      sem_getvalue(&barrier->sem, &semcount);
+      if (semcount == 0)
+        {
+          sem_destroy(&barrier->sem);
+          barrier->count = 0;
+        }
+      else
+        {
+          ret = EBUSY;
+        }
     }
 
   return ret;